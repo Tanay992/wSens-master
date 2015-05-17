546cce465c2387da6f2585a47c35e8d33a30ba19
Tanay <tanay992@ucla.edu>
2015-05-17 15:53:29 -0700

    StoreData added

---
+++ app/src/main/java/com/example/MainInterface/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.MainInterface;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+
+import com.example.bluetoothsensor.R;
+
+public class SplashScreen extends Activity{
+		@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash_screen);
+		
+		/*
+		 * This timer helps us detect when the device has been turned off. Ie.
+		 * if the last received data does not change for some time
+		 */
+		Log.d("SplashScreen", "in the oncreate");
+	        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                //Create an intent to launch the MainUI.
+                Intent mainIntent = new Intent(SplashScreen.this,MainUI.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, 2000);
+		
+    }
+	
+}
